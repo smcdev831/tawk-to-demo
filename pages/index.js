8df1eb804cecfd15796c360a83b7f43ea0710214
smcdev <smcdev@outlook.com>
2023-01-17 00:23:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,20 @@ export default function Home() {
       <main>
         <Header title="Welcome to the testing space for Tawk.to!" />
         <p className="description"></p>
+
+        <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62392991a34c2456412c2ed8/1funkkof1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
       </main>
 
       <Footer />
